352cd25fccdfafc078bf0c39ad48f1afa03bd37d
NovElena <novokhodskayaelena@gmail.com>
2026-01-16 10:04:45 +0200

    CssSelectors and Xpath

---
+++ src/test/java/SelectorsCss.java
@@ -0,0 +1,110 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SelectorsCss {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void phoneBookTest(){
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.get("http:telranedu.web.app/home");
+        driver.navigate().to("http://telranedu.web.app/home");
+        pause(3);
+        WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+        btnAbout.click();
+        //WebElement divRoot = driver.findElement(By.cssSelector("div[id='root]"));
+        WebElement divRoot = driver.findElement(By.id("root"));
+        System.out.println(divRoot.getAttribute("class"));
+        WebElement divRootClass = driver.findElement(By.cssSelector("div[class='container']"));
+        //Web
+        //Web
+        System.out.println(divRootClass.getAttribute("id"));
+        pause(3);
+        driver.navigate().refresh();
+        pause(3);
+        driver.navigate().to("https://telranedu.web.app/login");
+        pause(3);
+        driver.navigate().back();
+        //driver.close():
+        driver.quit();
+    }
+
+    @Test
+    public void ilCarroTest(){
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://ilcarro.web.app/search");
+        pause(3);
+        WebElement logoHeader = driver.findElement(By.cssSelector("img[alt='logo']"));
+        logoHeader.click();
+        pause(3);
+        //a.navigation-link
+        //a[class='navigation-link']
+        //.navigation-link
+        //a[class='navigation-link']
+        //*[class='navigation-link']
+        //[class='navigation-link']
+        WebElement btnSearch = driver.findElement(By.className("navigation-link"));
+        btnSearch.click();
+        pause(3);
+        
+        //a[id='1']
+        //a#1.navigation-link
+        //#1
+        //[id='1']
+        //WebElement btnLetCarWork = driver.findElement(By.cssSelector("#1"));
+        WebElement btnLetCarWork = driver.findElement(By.className("navigation-link"));
+        btnSearch.click();
+        pause(3);
+
+        //a[id='1']
+        //a#1.navigation-link
+        //#1
+        //[id='1']
+        //WebElement btnLetCarWork = driver.findElement(By.cssSelector("#1"));
+        //WebElement btnLetCarWork = driver.findElement(By.id("1"));
+        //WebElement btnLetCarWork = driver.findElement(By.linkText("Let the car work");
+        //WebElement btnLetCarWork = driver.findElement(By.partialLinkText("work"));
+        btnLetCarWork.click();
+        pause(3);
+
+
+        //a[href*='/terms'] include
+        //a[href^='/terms'] start
+        //a[href$='-use'] finish
+        //a.navigation-link[href='/terms-of-use']
+        WebElement btnTermOfUse = driver.findElement(By.cssSelector("[href='/terms-of-use']"));
+        btnTermOfUse.click();
+        pause(5);
+
+        WebElement btnSignUp = driver.findElement(By.cssSelector("div.header a.navigation-link:nth-child(5)"));
+        btnSignUp.click();
+        pause(5);
+
+        WebElement btnLogIn = driver.findElement(By.cssSelector("div.header a.navigation-link:last-child"));
+        btnLogIn.click();
+        pause(5);
+
+        driver.navigate().to("https://ilcarro.web.app/search");
+        driver.quit();
+
+
+    }
+
+    static void pause(int time){
+        try {
+            Thread.sleep(time * 1000L);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/SelectorsXpath.java
@@ -0,0 +1,96 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+
+public class SelectorsXpath {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void phonebookTest(){
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://telranedu.web.app/home");
+        //WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+        WebElement btnAbout = driver.findElement(By.xpath("//a[@href='/about']"));
+        // //*[@href='/about']
+        // //*[text()='ABOUT']
+        btnAbout.click();
+        //SelectorsCss.pause(3);
+        pause(3);
+
+        WebElement btnLogin = driver.findElement(By.xpath("//*[text()='LOGIN']"));
+        btnLogin.click();
+        WebElement fieldEmail = driver.findElement(By.xpath("//input[@placeholder='Email']"));
+        int i = new Random().nextInt(1000);
+        fieldEmail.sendKeys("qafisgfo"+i+"@gmail.com");
+        pause(3);
+
+        WebElement fieldPassword = driver.findElement(By.xpath("//*[@name='password']"));
+        fieldPassword.sendKeys("Password123!");
+        pause(3);
+
+        WebElement btnRegistration = driver.findElement(By.xpath("//*[@name='registration']"));
+        btnRegistration.click();
+        pause(3);
+        WebElement btnSignOut = driver.findElement(By.xpath("//button[text()='Sign Out']"));
+        btnSignOut.click();
+        pause(3);
+
+        List<WebElement> buttons = driver.findElements(By.xpath("//button"));
+        System.out.println(buttons);
+        System.out.println(buttons.get(1).getText());
+    }
+
+    @Test
+    public void iLcarroXpathTest(){
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://ilcarro.web.app/search");
+
+        WebElement linkLogin = driver.findElement(By.xpath("//a[text()='Log in']"));
+        linkLogin.click();
+        pause(3);
+
+        WebElement fieldEmail = driver.findElement(By.xpath("//input[@id='email']"));
+        fieldEmail.click();
+        fieldEmail.sendKeys("123qwe!@gmail.com");
+        pause(3);
+
+        WebElement fieldPassword = driver.findElement(By.xpath("//input[@id='password']"));
+        fieldPassword.click();
+        fieldPassword.sendKeys("123Qwerty!#");
+        pause(3);
+
+        WebElement btnYalla = driver.findElement(By.xpath("//button[@type='submit']"));
+        btnYalla.click();
+        pause(3);
+
+        WebElement btnOk = driver.findElement(By.xpath("//button[@class='positive-button ng-star-inserted']"));
+        btnOk.click();
+        pause(3);
+
+        WebElement btnLogout = driver.findElement(By.xpath("//a[contains(text(), 'Logout')]"));
+        btnLogout.click();
+        pause(3);
+
+        pause(10);
+
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time *1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
